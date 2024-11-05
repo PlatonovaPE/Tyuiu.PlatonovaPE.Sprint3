e664e978a5d14fe9e301a3eb18f060d2af1f597f
Полина Платонова <platonovaPE@outlook.com>
2024-11-05 17:43:37 +0500

    Task5

--- Tyuiu.PlatonovaPE.Sprint3.Task5.V20.Test/DataServiceTest.cs
+++ Tyuiu.PlatonovaPE.Sprint3.Task5.V20.Test/DataServiceTest.cs
@@ -1,3 +1,4 @@
+using Tyuiu.PlatonovaPE.Sprint3.Task5.V20.Lib;
 namespace Tyuiu.PlatonovaPE.Sprint3.Task5.V20.Test
 {
     [TestClass]
@@ -6,6 +7,20 @@ namespace Tyuiu.PlatonovaPE.Sprint3.Task5.V20.Test
         [TestMethod]
         public void TestMethod1()
         {
+            DataService ds = new DataService();
+
+            int x = 5;
+            int startValue1 = 1;
+            int stopValue1 = 3;
+            int startValue2 = 1;
+            int stopValue2 = 6;
+
+            double res = ds.GetSumSumSeries(x, startValue1, startValue2, stopValue1, stopValue2);
+
+            double wait = 50584.322;
+
+            Assert.AreEqual(res, wait);
+
         }
     }
 }
--- Tyuiu.PlatonovaPE.Sprint3.Task5.V20/Program.cs
+++ Tyuiu.PlatonovaPE.Sprint3.Task5.V20/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.ComponentModel;
+using System.ComponentModel.Design;
+using Tyuiu.PlatonovaPE.Sprint3.Task5.V20.Lib;
+
+DataService ds = new DataService();
+
+Console.WriteLine("********************************************************************************");
+Console.WriteLine("* Исходные данные                                                              *");
+Console.WriteLine("********************************************************************************");
+
+int x = 5;
+int startValue1 = 1;
+int stopValue1 = 3;
+int startValue2 = 1;
+int stopValue2 = 6;
+
+Console.WriteLine("Введите  значение X:" + x);
+Console.WriteLine("Старт шага:" + startValue1);
+Console.WriteLine("Конец шага:" + stopValue1);
+Console.WriteLine("Старт шага:" + startValue2);
+Console.WriteLine("Конец шага:" + stopValue2);
+
+
+Console.WriteLine("********************************************************************************");
+Console.WriteLine("* Результат:                                                                   *");
+Console.WriteLine("********************************************************************************");
+
+Console.WriteLine("Произведение ряда = " + ds.GetSumSumSeries(x, startValue1, startValue2, stopValue1, stopValue2));
+
+
+
+Console.ReadKey();
+
